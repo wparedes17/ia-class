66791aba77799ce7e3f6098e825579effae02dda
wparedes17 <wilfridojpg@data-developer.ru>
2023-02-13 17:22:07 -0600

    Add functions for punctuation

--- functions_to_call.py
+++ functions_to_call.py
@@ -1,3 +1,5 @@
+import re
+import unicodedata
 import numpy as np
 
 def hello_function():
@@ -7,29 +9,41 @@ def create_vocabulary(list_of_documents):
     i = 0
     vocabulary = {}
     # Para cada documento en la lista de documentos
-    for document in list_of_documents:
+    for document_no, document in enumerate(list_of_documents):
         # Separa el documento en palabras
-        words = document.split(' ')
+        words = set(document.split(' '))
         # Para cada palabra en las palabras
         for word in words:
             # Si la palabra no está en el vocabulario
+            # agregamos el id y el documento en el que aparece
             if word not in vocabulary:
-                vocabulary[word] = i
+                vocabulary[word] = {'id':i, 'docto':[document_no]}
                 i += 1
+            # en otro caso solo agregamos el documento en el que aparece
+            else:
+                vocabulary[word]['docto'].append(document_no)
+
+    # Calculamos el df (document frequency) para cada palabra
+    for word in vocabulary:
+        vocabulary[word]['df'] = len(vocabulary[word]['docto'])
+
     return vocabulary
 
-def create_vectorized_document(document, vocabulary):
+def create_vectorized_document(document, vocabulary, n_docs):
     # Separa el documento en palabras
     words = document.split(' ')
     # Crea un vector de ceros
     vector = np.zeros(len(vocabulary))
+    ivector = np.zeros(len(vocabulary))
     # Para cada palabra en las palabras
     for word in words:
         # Si la palabra está en el vocabulario
         if word in vocabulary:
             # Incrementa el valor del vector en la posición de la palabra
-            vector[vocabulary[word]] += 1
-    return vector
+            vector[vocabulary[word]['id']] += 1
+            ivector[vocabulary[word]['id']] = n_docs/vocabulary[word]['df']
+    
+    return (np.log(vector+1))*ivector
 
 def create_matrix_documents(list_of_documents, vocabulary):
     # Crea una matriz de ceros
@@ -37,7 +51,7 @@ def create_matrix_documents(list_of_documents, vocabulary):
     # Para cada documento en la lista de documentos
     for i in range(len(list_of_documents)):
         # Crea un vector para el documento
-        vector = create_vectorized_document(list_of_documents[i], vocabulary)
+        vector = create_vectorized_document(list_of_documents[i], vocabulary, len(list_of_documents))
         # Agrega el vector a la matriz
         matrix[i] = vector
     return matrix
@@ -54,7 +68,7 @@ def calculate_cosine_similarity(vector1, vector2):
 
 def calculate_similarity_for_query(query, matrix_documents, vocabulary):
     # Crea un vector para la consulta
-    vector_query = create_vectorized_document(query, vocabulary)
+    vector_query = create_vectorized_document(query, vocabulary, 10)
     # Crea una lista para las similitudes
     similarities = {}
     # Para cada documento en la matriz de documentos
@@ -70,5 +84,13 @@ def sort_dictionary_by_value(dictionary):
     sorted_dictionary = {k: v for k,v in sorted(dictionary.items(), key=lambda x: x[1], reverse=True)}
     return sorted_dictionary
 
+def remove_accents(input_str):
+    if isinstance(input_str, str):
+        nkfd_form = unicodedata.normalize('NFKD', input_str.lower())
+        return u"".join([c for c in nkfd_form if not unicodedata.combining(c)])
+    return str(input_str)
 
-# DF-IDF = (1 + log(tf)) * log(N/df)
+def remove_punctuation(input_str):
+    if isinstance(input_str, str):
+        return re.sub(r'[^\w\s]', ' ', input_str)
+    return str(input_str)
